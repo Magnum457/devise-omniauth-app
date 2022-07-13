9a9255a70f11e53f75d41e073b3123c92c983338
Magno Felipe <mfts201116@gmail.com>
2022-07-13 09:21:05 -0300

    Configure the devise.rb to use the OAuth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,7 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :digitalocean, "db381dc9990be7e3bc42503d0", "5b0824c2722b65d29965f1a1df"
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
